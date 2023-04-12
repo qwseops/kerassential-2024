f40ec045d80676fc3750119819104417f67002fc
qwseops <40327168+qwseops@users.noreply.github.com>
2023-04-12 17:43:32 +0500

    Update get.php

--- get.php
+++ get.php
@@ -1,3 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11141310153"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11141310153');
+</script>
 <?php
 
 header("Location:https://vclit.com/chummyburn")
